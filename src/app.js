63f8b00649a5297af3f3baa84d54d349c652e9f8
chichoinf@gmail.com <chichoinf@gmail.com>
2026-05-28 16:40:48 -0300

    base terminada (espero)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 const app = express();
 
 
@@ -10,9 +11,26 @@ app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.get('/',(req,res)=>{
-    res.render('index');
+app.use(session({
+    store: new pgSession({
+        conObject:{
+            user:process.env.DB_USER,
+            host:process.env.DB_HOST,
+            database:process.env.DB_NAME,
+            password:process.env.DB_PASSWORD,
+            port:process.env.DB_PORT
+        }
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
 
-});
+
+app.use('/',require('./routes/home.routes'));
+app.use('/auth',require('./routes/auth.routes'));
 
 module.exports = app;
