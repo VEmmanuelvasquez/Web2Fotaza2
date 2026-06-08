43f2a31206dcab93a147ea17238b1f7f68091347
chichoinf@gmail.com <chichoinf@gmail.com>
2026-06-08 17:54:25 -0300

    nmp run db:init

---
+++ database/init.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const sequelize = require('../src/config/db');
+
+require('../src/models/Usuario');
+require('../src/models/Publicacion');
+require('../src/models/Comentario');
+require('../src/models/Valoracion');
+require('../src/models/Seguimiento');
+require('../src/models/Interes');
+require('../src/models/Favorito');
+require('../src/models/Coleccion');
+require('../src/models/ColeccionPublicacion');
+require('../src/models/Notificacion');
+require('../src/models/Denuncia');
+
+sequelize.sync({ force: true })
+.then(() => {
+    console.log('Base de datos inicializada correctamente');
+    process.exit(0);
+})
+.catch(error => {
+    console.log(error);
+    process.exit(1);
+});
