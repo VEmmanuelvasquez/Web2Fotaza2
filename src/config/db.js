d91983fba87602a50f62b78257e3b03e7572d4b2
chichoinf@gmail.com <chichoinf@gmail.com>
2026-06-12 22:12:56 -0300

    neon correguido

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+const config = {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT
+};
+
+if (process.env.DB_HOST.includes('neon.tech')) {
+    config.dialectOptions = {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    };
+}
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        port: process.env.DB_PORT,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
-    }
+    config
 );
 
 module.exports = sequelize;
